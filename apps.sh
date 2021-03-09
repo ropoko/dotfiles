98fc6f6808dc5f2631ec14e504b8f91921e998af
Rodrigo Stramantinoli <49417432+ropoko@users.noreply.github.com>
2021-03-08 21:04:50 -0300

    Update apps.sh

--- apps.sh
+++ apps.sh
@@ -19,10 +19,10 @@ cd /home/$USER/Downloads
 
 # sublime - download and installation
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add - &&
-sudo apt-get install apt-transport-https &&
+sudo apt install apt-transport-https &&
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list &&
-sudo apt-get update &&
-sudo apt-get install sublime-text;
+sudo apt update &&
+sudo apt install sublime-text;
 
 # qbittorrent
 sudo add-apt-repository ppa:qbittorrent-team/qbittorrent-stable &&
