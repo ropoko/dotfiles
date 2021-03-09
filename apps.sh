5844244d21e8f980f3f45c949bb5aa87b05429c3
ropoko <rodrigostramantinoli@gmail.com>
2021-03-08 21:02:17 -0300

    added setup files

---
+++ apps.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+# Installing snaps
+sudo apt install snapd &&
+
+sudo snap install cli-dictionary;
+sudo snap install code --classic;
+sudo snap install discord;
+sudo snap install leagueoflegends --devmode --dangerous;
+sudo snap install notion-snap;
+sudo snap install review-tools;
+sudo snap install spotify;
+sudo snap install telegram-desktop;
+sudo snap install strimio-desktop;
+
+# programs with apt
+
+cd /home/$USER/Downloads
+
+# sublime - download and installation
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add - &&
+sudo apt-get install apt-transport-https &&
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list &&
+sudo apt-get update &&
+sudo apt-get install sublime-text;
+
+# qbittorrent
+sudo add-apt-repository ppa:qbittorrent-team/qbittorrent-stable &&
+sudo apt install qbittorrent;
+
+# vivaldi - download
+wget -c https://downloads.vivaldi.com/stable/vivaldi-stable_3.6.2165.40-1_amd64.deb;
+
+# edge - download
+wget -c https://go.microsoft.com/fwlink/?linkid=2124602;
+
+# krita
+wget -c https://download.kde.org/stable/krita/4.4.2/krita-4.4.2-x86_64.appimage &&
+sudo chmod u+x *.appimage;
+
+# steam
+sudo apt install steam;
+
+# caffeine 
+sudo apt install caffeine;
+
+# font-manager
+sudo apt install font-manager;
+
+# Installing .deb files
+sudo dpkg -i *.deb;
+
+# Removing apps 
+sudo apt remove firefox;
+
+sudo apt remove gedit;
+
+# Configuration file
+sudo chmod +x ./configuration.sh &&
+./configuration.sh;
---
+++ configuration.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Installing GIT
+sudo apt install git;
+
+## Configuring GIT
+git config --global user.name "ropoko";
+git config --global user.email rodrigostramantinoli@gmail.com;
+git config --global init.defaultBranch main;
+
+# Installing ZSH
+sudo apt install zsh && chsh -s $(which zsh);
+
+sudo apt update && sudo apt upgrade
+
+## Installing oh-my-zsh
+wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh &&
+sh install.sh;
+
+# Installing ASDF
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.8.0;
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf;
+cd ~/.asdf;
+git checkout "$(git describe --abbrev=0 --tags)";
+
+# Adding plugins to ZSH
+sudo apt install sed;
+
+if grep -q "plugin=(git)"; then
+    sed -i 's/plugin=(git)/plugins=(git sudo asdf virtualenv)/g' ~/.zshrc;
+else
+    echo "some plugins may have to be added manually"
+fi
+
+source ~/.zshrc;
