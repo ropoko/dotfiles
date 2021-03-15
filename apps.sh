9ea535a3ea4b9e9fa708048e7a37503789f275ce
ropoko <rodrigostramantinoli@gmail.com>
2021-03-15 15:35:39 -0300

    added zshrc

--- apps.sh
+++ apps.sh
@@ -1,80 +1,76 @@
 #!/bin/bash
 
-sudo apt update -y && apt upgrade -y &&
+sudo apt update -y && sudo apt list --upgradable && sudo apt upgrade -y
 
-sudo rm /var/lib/dpkg/lock-frontend ; sudo rm /var/cache/apt/archives/lock;
+sudo rm /var/lib/dpkg/lock-frontend
+sudo rm /var/cache/apt/archives/lock
 
-sudo dpkg --configure -a &&
+sudo dpkg --configure -a
 
 # Installing snaps
-sudo apt install snapd -y &&
-
-sudo snap install cli-dictionary &&
-sudo snap install code --classic &&
-sudo snap install discord &&
-sudo snap install notion-snap &&
-sudo snap install review-tools &&
-sudo snap install telegram-desktop &&
-sudo snap install strimio-desktop &&
-sudo snap install mailspring &&
+sudo apt install snapd -y
+sudo snap install code --classic
+sudo snap install discord
+sudo snap install notion-snap
+sudo snap install review-tools
+sudo snap install telegram-desktop
+sudo snap install strimio-desktop
+sudo snap install mailspring
 
 # programs with apt
 
 cd /home/$USER/Downloads
 
 # spotify - spicetify
-curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | sudo apt-key add - 
-;echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
-;sudo apt-get update && sudo apt-get install spotify-client
+curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | sudo apt-key add -
+echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+sudo apt-get update && sudo apt-get install spotify-client
 
-;sudo chmod a+wr /usr/share/spotify
-;sudo chmod a+wr /usr/share/spotify/Apps -R
-;curl -fsSL https://raw.githubusercontent.com/khanhas/spicetify-cli/master/install.sh | sh
-;/home/$USER/spicetify-cli/spicetify backup apply
-;/home/$USER/spicetify-cli/spicetify  config current_theme Dracula
-;/home/$USER/spicetify-cli/spicetify apply
-;
+sudo chmod a+wr /usr/share/spotify
+sudo chmod a+wr /usr/share/spotify/Apps -R
+curl -fsSL https://raw.githubusercontent.com/khanhas/spicetify-cli/master/install.sh | sh
+/home/$USER/spicetify-cli/spicetify backup apply
+/home/$USER/spicetify-cli/spicetify  config current_theme Dracula
+/home/$USER/spicetify-cli/spicetify apply
 
 # sublime - download and installation
-wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add - &&
-sudo apt install -y apt-transport-https &&
-echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list &&
-sudo apt update -y &&
-sudo apt install sublime-text -y;
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+sudo apt install -y apt-transport-https
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sudo apt update -y
+sudo apt install sublime-text -y
 
 # qbittorrent
-sudo add-apt-repository ppa:qbittorrent-team/qbittorrent-stable -y &&
-sudo apt install qbittorrent -y;
+sudo add-apt-repository ppa:qbittorrent-team/qbittorrent-stable -y
+sudo apt install qbittorrent -y
 
 # vivaldi - download
-wget -c https://downloads.vivaldi.com/stable/vivaldi-stable_3.6.2165.40-1_amd64.deb;
+wget -c https://downloads.vivaldi.com/stable/vivaldi-stable_3.6.2165.40-1_amd64.deb
 
 # edge - download
-wget -c https://go.microsoft.com/fwlink/?linkid=2124602;
+wget -c https://go.microsoft.com/fwlink/?linkid=2124602
 
 # krita
-wget -c https://download.kde.org/stable/krita/4.4.2/krita-4.4.2-x86_64.appimage &&
-sudo chmod u+x *.appimage;
+wget -c https://download.kde.org/stable/krita/4.4.2/krita-4.4.2-x86_64.appimage
+sudo chmod u+x *.appimage
 
 # steam
-sudo apt install steam -y;
+sudo apt install steam -y
 
 # caffeine 
-sudo apt install caffeine -y;
+sudo apt install caffeine -y
 
 # font-manager
-sudo apt install font-manager -y;
+sudo apt install font-manager -y
 
-wget -c https://download.virtualbox.org/virtualbox/6.1.18/virtualbox-6.1_6.1.18-142142~Ubuntu~eoan_amd64.deb;
+wget -c https://download.virtualbox.org/virtualbox/6.1.18/virtualbox-6.1_6.1.18-142142~Ubuntu~eoan_amd64.deb
 
 # Installing .deb files
-sudo dpkg -i *.deb;
+sudo dpkg -i *.deb
 
 # Removing default apps 
-sudo apt remove firefox -y;
-
-sudo apt remove gedit -y;
+sudo apt remove firefox -y && sudo apt remove gedit -y
 
 # Configuration file
-sudo chmod +x ./configuration.sh &&
-./configuration.sh;
+sudo chmod +x ./configuration.sh
+./configuration.sh
--- configuration.sh
+++ configuration.sh
@@ -1,37 +1,28 @@
 #!/bin/bash
 
-sudo apt autoremove -y && apt clean;
+sudo apt autoremove -y && apt clean
 
 # Installing GIT
-sudo apt install git -y;
+sudo apt install git -y
 
 ## Configuring GIT
-git config --global user.name "ropoko";
-git config --global user.email rodrigostramantinoli@gmail.com;
-git config --global init.defaultBranch main;
+git config --global user.name "ropoko"
+git config --global user.email rodrigostramantinoli@gmail.com
+git config --global init.defaultBranch main
 
 # Installing ZSH
-sudo apt install zsh -y && chsh -s $(which zsh);
+sudo apt install zsh -y && chsh -s $(which zsh)
 
-sudo apt update -y && sudo apt upgrade -y &&
+sudo apt update -y && sudo apt upgrade -y
 
 ## Installing oh-my-zsh
-wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh &&
-sh install.sh;
+wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
+sh install.sh
+
+source ~/.zshrc
 
 # Installing ASDF
-git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.8.0;
-git clone https://github.com/asdf-vm/asdf.git ~/.asdf;
-cd ~/.asdf;
-git checkout "$(git describe --abbrev=0 --tags)";
-
-# Adding plugins to ZSH
-sudo apt install sed -y;
-
-if grep -q "plugin=(git)"; then
-    sed -i 's/plugin=(git)/plugins=(git sudo asdf virtualenv)/g' ~/.zshrc;
-else
-    echo "some plugins may have to be added manually"
-fi
-
-source ~/.zshrc;
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.8.0
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf
+cd ~/.asdf
+git checkout "$(git describe --abbrev=0 --tags)"
