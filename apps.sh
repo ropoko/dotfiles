738b22c90bc14a7dd63bf44ccce12387ecdc42bd
ropoko <rodrigostramantinoli@gmail.com>
2021-06-04 19:52:04 -0300

    sublime removed

--- apps.sh
+++ apps.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-sudo apt update -y && sudo apt list --upgradable && sudo apt upgrade -y
+sudo apt update -y
+sudo apt list --upgradable
+sudo apt upgrade -y
 
 sudo dpkg --configure -a
 
@@ -17,13 +19,6 @@ sudo snap install mailspring
 # programs with apt
 
 cd /home/$USER/Downloads
-
-# sublime - download and installation
-wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-sudo apt install -y apt-transport-https
-echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-sudo apt update -y
-sudo apt install sublime-text -y
 
 # qbittorrent
 sudo add-apt-repository ppa:qbittorrent-team/qbittorrent-stable -y
