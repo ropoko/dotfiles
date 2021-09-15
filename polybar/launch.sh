262ec635c5f37f7f3ba43241dd9e620fec191475
ropoko <rodrigostramantinoli@gmail.com>
2021-09-15 20:49:06 +0000

    added new bar with tray

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -5,11 +5,12 @@ killall -q polybar
 # If all your bars have ipc enabled, you can also use 
 # polybar-msg cmd quit
 
-# Launch bar1 and bar2
+# Launch all bars
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
 
 polybar bar1 2>&1 | tee -a /tmp/polybar1.log & disown
 polybar bar2 2>&1 | tee -a /tmp/polybar2.log & disown
 polybar bar3 2>&1 | tee -a /tmp/polybar3.log & disown
+polybar bar4 2>&1 | tee -a /tmp/polybar4.log & disown
 
 echo "starting polybar..."
